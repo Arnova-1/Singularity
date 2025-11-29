2a8796eb5379917615e624799d2c6a2a5c56edae
Arnova-1 <arnova124@gmail.com>
2025-11-29 08:59:07 +0700

    feat: implement basic UEFI hello world example

--- Bootloader/src/main.rs
+++ Bootloader/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+#![no_main]
+#![no_std]
+
+use core::time::Duration;
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+
+    info!("Hello!");
+
+    boot::stall(Duration::from_secs(10));
+
+    Status::SUCCESS
+}
